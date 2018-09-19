a575a5f55bce416711dc4be5d443ae5df7d9946d
untungtanujaya <utanujaya@gmail.com>
2018-09-19 20:03:08 +0700

    add app.py and nything.py

---
+++ app.py
@@ -0,0 +1,2 @@
+# Name : app.py
+# Desc : read the txt file (fileInput), then make object nything.
---
+++ nything.py
@@ -0,0 +1,19 @@
+# Name : problemSolver.py
+# Desc : file of class nything, to solve NythingProblem
+
+class nything:
+    # attribute
+
+
+    # method
+    def __init__(self, fileInput):
+        # put fileInput (in .txt) to attribute
+
+    def hillClimbing(self):
+        # solve using hillClimbing
+
+    def simulatedAnnealing(self):
+        # solve using simulatedAnnealing
+
+    def geneticAlgorithm(self):
+        #  solve using geneticAlgorithm
